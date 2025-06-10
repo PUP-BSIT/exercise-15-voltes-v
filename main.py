09e59d27ba940b7fc29d88dd9d1529e827aa6091
Gulles-Althea Shane <gullesalthea00@gmail.com>
2025-06-11 00:11:45 +0800

    add package and main.py

---
+++ main.py
@@ -0,0 +1,33 @@
+choice = 0
+while choice != 6:
+    print("-------------------------------------------")
+    print("            Voltes V Main Menu             ") 
+    print("-------------------------------------------")
+    print("1. John Cris Caculitan ")
+    print("2. Precious Hannah Corpus ")
+    print("3. Althea Shane Gulles ")
+    print("4. Woman Daphne Morales ")
+    print("5. Dyanna May Pineda ")
+    print("6. Exit")
+    try:
+        choice = int(input("Enter your choice (1-6): "))
+    except ValueError:
+        print("Please enter a valid number.")
+        continue
+    print("-------------------------------------------")
+
+    match choice:
+        case 1: # TODO: Create a class and import your module here
+            pass
+        case 2: # TODO: Create a class and import your module here
+            pass
+        case 3: # TODO: Create a class and import your module here
+            pass
+        case 4: # TODO: Create a class and import your module here
+            pass
+        case 5: # TODO: Create a class and import your module here
+            pass
+        case 6: # TODO: Handle program exit if needed
+            pass
+        case _: # TODO: Handle invalid input
+            pass
